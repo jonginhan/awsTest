0f6fd375f3146d95fec13617e48029220ae272d5
jonginhan <instantgogo@gmail.com>
2023-04-18 21:48:14 +0900

    init:[한종인] 프로젝트 초기화

---
+++ demo 2/src/main/java/com/example/demo/awsController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class awsController {
+
+        @GetMapping("/hi")
+        public String aws() {
+            return "hi";
+        }
+}
